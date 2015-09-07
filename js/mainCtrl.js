71f4b2a42e986c1fcc96d183c8d9faa56f1b9b43
Todd Hayes <todd@i.tv>
2015-09-07 13:02:52 -0600

    "add friend"

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -3,4 +3,12 @@ var app = angular.module('friendsList');
 app.controller('mainCtrl', function($scope){
 	$scope.friends = ["Eric", "Elias", "Robb", "Ben"];
 
+
+
+$scope.friendAdded = function() {
+	var frnd = $scope.newFriend;
+	$scope.friends.push(frnd);
+	$scope.newFriend = '';
+}
+
 });
